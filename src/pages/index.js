50c061ce377cc2513a555c52ab38079ef5a681e7
hamza-kay <hamzahk1991@gmail.com>
2019-05-03 03:37:31 +0100

    final gatsby site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,23 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
 
-export default () => <div>Hello world!</div>
+import Layout from '../components/layout'
+import Head from '../components/head'
+
+const IndexPage = () => {
+    return (
+        
+            <Layout>
+            <Head title="Home" />
+
+            <h1>Hello</h1>
+            <h2>I'm Hamza, A developer.</h2>
+            <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
+
+            </Layout>
+
+            )
+        }
+  
+
+export default IndexPage
---
+++ src/posts/react/react.md
@@ -0,0 +1,6 @@
+---
+title: "React"
+date: "20-04-2019"
+---
+
+In this post you'll learn React.
